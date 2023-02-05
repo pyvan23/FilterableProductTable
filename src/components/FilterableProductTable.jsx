aad10ec419dee7d4425ccfd177d89b8aa581c330
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-02-05 10:20:39 -0300

    commnets props one way

--- src/components/FilterableProductTable.jsx
+++ src/components/FilterableProductTable.jsx
@@ -4,6 +4,13 @@ import SearchBar from './SearchBar';
 
  export default function FilterableProductTable({ products }) {
 
+//ProductTable necesita filtrar la lista de productos con base en ese estado (texto de búsqueda y valor del checkbox).
+//SearchBar necesita mostrar ese estado (texto de búsqueda y valor del checkbox).
+//Encuentra su ancestro común: El primer componente ancestro que ambos componentes comparten es FilterableProductTable.
+//Decide donde reside el estado: Mantendremos el texto de filtrado y el estado de valor seleccionado en FilterableProductTable.
+
+
+
   const [filterText, setFilterText] = useState('');
   const [inStockOnly, setInStockOnly] = useState(false);
 
